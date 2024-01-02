715e4ecfaa2f04f9ef73531cd0117db85af7c717
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-02 10:28:29 -0500

    reformat: removed white space

--- src/features/creatures-page/CreaturesPage.tsx
+++ src/features/creatures-page/CreaturesPage.tsx
@@ -3,12 +3,11 @@ import Header from "@/components/header/Header";
 import CreaturesList from "../creatures-list/CreaturesList";
 
 export default function CreaturesPage() {
-    return (
-        <>
-            <Header pageName={"Creatures"} searchPlaceHolder={"Search Creatures"} />
-
-            <CreaturesList />
-            <Nav />
-        </>
-    );
+	return (
+		<>
+			<Header pageName={"Creatures"} searchPlaceHolder={"Search Creatures"} />
+			<CreaturesList />
+			<Nav />
+		</>
+	);
 }
