16ed2b9c0fbdff6505c60fa1c4e2a9ae75d6a608
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-08 15:11:42 -0500

    Fix: useDisableBodyScroll to still show scrollbar but disable scrolling

--- src/hooks/useDisableBodyScroll.tsx
+++ src/hooks/useDisableBodyScroll.tsx
@@ -3,15 +3,16 @@ import { useRef, useEffect } from "react";
 // This hook is designed to simplify the process of disabling the scroll on the body element typically used in conjunction with modals. This hook utilizes a useRef for the body element and a useEffect to apply overflow styling upon the first render. It then returns a cleanup arrow function within the useEffect to remove the scroll lock when needed.
 
 export default function useDisableBodyScroll() {
-    const bodyEle = document.body;
-    const bodyRef = useRef(bodyEle);
+	const bodyEle = document.body;
+	const bodyRef = useRef(bodyEle);
 
-    useEffect(() => {
-        // disable scroll when modal is open
-        bodyRef.current.style.overflow = "hidden";
-        // remove overflow-hidden when modal closed
-        return () => {
-            bodyRef.current.style.overflow = "scroll";
-        };
-    }, [bodyRef]);
+	useEffect(() => {
+		// Fixed scroll when modal is open
+		bodyRef.current.style.position = "fixed";
+		bodyRef.current.style.width = "100%";
+		// remove overflow-hidden when modal closed
+		return () => {
+			bodyRef.current.style.position = "static";
+		};
+	}, [bodyRef]);
 }
