e618d97a25e4495cd9a0472a33197bb43ca55801
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-08 13:52:27 -0500

    import createRoot and check root if it's null

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./style/index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
+const rootElement = document.getElementById("root");
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <App />
-        </QueryClientProvider>
-    </React.StrictMode>
+if (!rootElement) {
+	throw new Error("Root element not found");
+}
+
+const root = createRoot(rootElement);
+
+root.render(
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<App />
+		</QueryClientProvider>
+	</React.StrictMode>
 );
