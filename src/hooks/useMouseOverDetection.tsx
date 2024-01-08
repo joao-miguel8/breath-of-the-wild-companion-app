9ea463ca172cf7ad267bb0143586915db285c67a
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-08 11:19:30 -0500

    Fix: type errors and inital values
    Fix - globalCordXY default values to 0
    Change - elementBoundaries x and y from globalCordXY to elementChosen
    Add - typescript generic to chosenEle as HTMLElement

--- src/hooks/useMouseOverDetection.tsx
+++ src/hooks/useMouseOverDetection.tsx
@@ -1,36 +1,37 @@
 import { useRef, useState, useEffect } from "react";
 
 export default function useMouseOverDetection() {
-    const [globalCordXY, setGlobalCordXY] = useState({ x: null, y: null });
-    const [isHovered, setIsHovered] = useState(false);
-    const chosenEle = useRef(null);
+	const [globalCordXY, setGlobalCordXY] = useState({ x: 0, y: 0 });
+	const [isHovered, setIsHovered] = useState(false);
+	const chosenEle = useRef<HTMLElement>(null);
 
-    const elementBoundaries = (x: number, left: number, right: number, y: number, top: number, bottom: number) => {
-        setIsHovered(x >= left && x <= right && y >= top && y <= bottom);
-    };
+	const elementBoundaries = (x: number, left: number, right: number, y: number, top: number, bottom: number) => {
+		setIsHovered(x >= left && x <= right && y >= top && y <= bottom);
+	};
 
-    function handleGlobalMouseCords() {
-        if (chosenEle.current === null) {
-            return;
-        }
-        const elementChosen = chosenEle.current.getBoundingClientRect();
-        elementBoundaries(globalCordXY.x, elementChosen.left, elementChosen.right, globalCordXY.y, elementChosen.top, elementChosen.bottom);
-    }
-    useEffect(() => {
-        const updateCords = e => {
-            setGlobalCordXY({ x: e.x, y: e.y });
-        };
-        handleGlobalMouseCords();
-        window.addEventListener("mousemove", updateCords);
+	function handleGlobalMouseCords() {
+		if (chosenEle.current === null) {
+			return;
+		}
+		const elementChosen = chosenEle.current.getBoundingClientRect();
+		elementBoundaries(elementChosen.x, elementChosen.left, elementChosen.right, elementChosen.y, elementChosen.top, elementChosen.bottom);
+	}
+	useEffect(() => {
+		const updateCords = (e: MouseEvent) => {
+			setGlobalCordXY({ x: e.x, y: e.y });
+		};
+		handleGlobalMouseCords();
+		window.addEventListener("mousemove", updateCords);
 
-        return () => {
-            window.removeEventListener("mousemove", updateCords);
-        };
-    }, [globalCordXY]);
-    return {
-        chosenEle,
-        isHovered,
-        setIsHovered,
-        globalCordXY: globalCordXY,
-    };
+		return () => {
+			window.removeEventListener("mousemove", updateCords);
+		};
+	}, [globalCordXY]);
+
+	return {
+		chosenEle,
+		isHovered,
+		setIsHovered,
+		globalCordXY: globalCordXY,
+	};
 }
