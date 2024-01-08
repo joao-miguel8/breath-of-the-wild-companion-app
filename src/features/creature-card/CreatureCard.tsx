bcab67563112e5b02282973811b46c983b7b9385
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-08 10:14:02 -0500

    Fix: type errors in CreatureCard

--- src/features/creature-card/CreatureCard.tsx
+++ src/features/creature-card/CreatureCard.tsx
@@ -1,12 +1,14 @@
 import { creatureCardType } from "./types/creatureType";
 
-export default function CreatureCard({ name, imageURL, key }: creatureCardType) {
-    return (
-        <div aria-label="click for more information" key={key} className="w-60 flex flex-col bg-[#181818] rounded-md overflow-hidden cursor-pointer hover:scale-105 ease-in-out transition duration-300">
-            <img src={imageURL} alt={name} className="hover:opacity-60 hover: duration-300" />
-            <div className="p-2">
-                <h4 className="text-20 text-center text-[#FAF7DC] font-breath">{name}</h4>
-            </div>
-        </div>
-    );
+function CreatureCard({ name, imageURL }: { name: creatureCardType["name"]; imageURL: creatureCardType["imageURL"] }) {
+	return (
+		<div aria-label="click for more information" className="w-60 relative flex flex-col bg-[#181818] rounded-md overflow-hidden cursor-pointer hover:scale-105 ease-in-out transition duration-300">
+			<img src={imageURL} alt={name} className="hover:opacity-60 hover: duration-300" />
+			<div className="p-2">
+				<h4 className="text-20 text-center text-[#FAF7DC] font-breath">{name}</h4>
+			</div>
+		</div>
+	);
 }
+
+export default CreatureCard;
